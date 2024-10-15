5105126bab46540d420323f4ead65154c8749d83
Pieter Wolmarans <wolliepiet@gmail.com>
2024-10-15 22:24:46 +0200

    fix: config and styling

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
