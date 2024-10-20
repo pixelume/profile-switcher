319c2733f9c89093ce44a483cb534aaebbfab9c5
Pieter Wolmarans <wolliepiet@gmail.com>
2024-10-20 07:24:53 +0200

    chore: add urls to env vars

--- client/src/lib/apollo-client.ts
+++ client/src/lib/apollo-client.ts
@@ -1,34 +1,34 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
-import { setContext } from '@apollo/client/link/context';
-
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
+import { GRAPHQL_BASE_URL } from "./constants";
 // Error handling link
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.forEach(({ message, locations, path }) =>
       console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
     );
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
 // HTTP link
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql', // Replace with your GraphQL API endpoint
+  uri: GRAPHQL_BASE_URL, // Replace with your GraphQL API endpoint
 });
 
 // Auth link for adding headers
 const authLink = setContext((_, { headers }) => {
   // Get the authentication token from local storage if it exists
-  const token = localStorage.getItem('auth_token');
+  const token = localStorage.getItem("auth_token");
   // Return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+    },
+  };
 });
 
 // Cache configuration
@@ -48,15 +48,15 @@ export const client = new ApolloClient({
   cache: cache,
   defaultOptions: {
     watchQuery: {
-      fetchPolicy: 'cache-and-network',
-      errorPolicy: 'ignore',
+      fetchPolicy: "cache-and-network",
+      errorPolicy: "ignore",
     },
     query: {
-      fetchPolicy: 'network-only',
-      errorPolicy: 'all',
+      fetchPolicy: "network-only",
+      errorPolicy: "all",
     },
     mutate: {
-      errorPolicy: 'all',
+      errorPolicy: "all",
     },
   },
 });
--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,1 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const GRAPHQL_BASE_URL = import.meta.env.VITE_GRAPHQL_BASE_URL;
